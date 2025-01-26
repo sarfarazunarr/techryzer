9f351e17f0831b7a04e055fb4dbcb7979bc72acf
Sarfaraz Unar <sarfarazunarr@gmail.com>
2025-01-26 15:26:42 +0500

    portfolio updated

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -1,17 +1,52 @@
-const projectsdata = [
+const projects = [
     {
         img: "/projects/hekto.jpg",
-        title: "Hekto - Ecommerce",
-        description: "It is a full stack ecommerce website with cart, checkout, user authentication created with NextJS and Sanity CMS.",
+        title: "Hekto Ecommerce",
+        description: "It is a full ecommerce website where users can browse products, add to cart, checkout and can place order. Currently it does not have admin panel that will be added soon.",
         github: "https://github.com/sarfarazunarr/hekto-ecommerce",
         video: "https://hekto-ecommerce-web.vercel.app/",
     },
     {
-        img: "/projects/livelap.jpg",
-        title: "Livelap - Ecommerce",
-        description: "It is a full stack ecommerce website with stripe payment, cart, checkout, user authentication created with PHP. It contains dashboard for vendor and admin.",
-        github: "https://github.com/sarfarazunarr/livelap-ecommerce-website",
-        video: "https://www.linkedin.com/posts/sarfarazunar_webdevelopment-ecommerce-php-activity-7228013324569833472-9y5L?utm_source=share&utm_medium=member_desktop",
+        img: "/projects/propertytax.jpg",
+        title: "Property Tax Calculator",
+        description: "Here is a amazing tool that will help you to calculate taxes on your property registration based on your provided location area.",
+        github: "https://github.com/sarfarazunarr/property-registration-tax-calculator",
+        video: "https://property-tax-calculator.vercel.app/",
+    },
+    {
+        img: "/projects/techstars.jpg",
+        title: "Techstars",
+        description: "Here is a complete blog frontend design that fetches data from sanity and this blog contain sanity cms. It has amazing design that is also replicated from figma.",
+        github: "https://github.com/sarfarazunarr/techstars",
+        video: "https://techstars-wine.vercel.app/",
+    },
+    {
+        img: "/projects/khareedlo.jpg",
+        title: "Khareedlo",
+        description: "It is a full ecommerce website where users can browse products, add to cart, checkout and can place order. Currently it does not have admin panel.",
+        github: "https://github.com/sarfarazunarr/khareedlo",
+        video: "https://khareedlo.vercel.app/",
+    },
+    {
+        img: "/projects/infotech.jpg",
+        title: "Infotech",
+        description: "Here is a complete company website that also contain team panel that has all important pages that a company website should have. It contain blogs, user management, project management features.",
+        github: "https://github.com/sarfarazunarr",
+        video: "https://infotechsolutions.cloud/",
+    },
+    {
+        img: "/projects/resumebuilder.jpg",
+        title: "Resume Builder",
+        description: "Here is a resume builder where you can easily create resumes and downlod in PDF Format just by filling out a form. This is part of hackathon 1",
+        github: "https://github.com/sarfarazunarr/static_resume_builder",
+        video: "https://static-resume-builder-seven-xi.vercel.app/",
+    },
+    {
+        img: "/projects/uitask.jpg",
+        title: "Landing Page Design",
+        description: "Here is a sample page created in NextJS by replicating Figma Design. This task was assigned during the learning phase of NextJS",
+        github: "https://github.com/sarfarazunarr/figma-to-nextjs",
+        video: "https://figma-to-nextjs-peach.vercel.app/",
     },
     {
         img: "/projects/invoicy.jpg",
@@ -19,6 +54,13 @@ const projectsdata = [
         description: "It is a invoicy app where user can register, login, and manage invoices it is a laravel based app. User can generate pdf of invoice and can send it to customer. User can also view all the invoices and can filter them by status. It is a full stack app created with laravel. It contains dashboard for admin and vendor.",
         github: "https://github.com/sarfarazunarr/invoicy",
         video: "https://www.linkedin.com/posts/sarfarazunar_laravel-crud-authentication-activity-7228364008334999552-i8YW?utm_source=share&utm_medium=member_desktop",
+    },
+    {
+        img: "/projects/notepadai.jpg",
+        title: "Notepad AI",
+        description: "I have redesigned Notepad using Tkinter & Python where i have integrated ChatGPT API. Where users can generate at content instantly inside notepad.",
+        github: "https://github.com/sarfarazunarr/notepad-ai",
+        video: "https://www.linkedin.com/posts/sarfarazunar_gpt-ai-g4f-activity-7228113152545067009-Oz-9?utm_source=share&utm_medium=member_desktop",
     },
     {
         img: "/projects/javascriptai.jpg",
@@ -43,6 +85,20 @@ const projectsdata = [
         video: "https://www.linkedin.com/posts/sarfarazunar_schoolmanagementsystem-techinnovation-qtcpp-activity-7220474766049087488-JV18?utm_source=share&utm_medium=member_desktop",
     },
     {
+        img: "/projects/typescript.jpg",
+        title: "10 TypeScript Projects",
+        description: "Here are 10 Typescript and nodejs projects assigned during the course of TypeScript at GIAIC.",
+        github: "https://github.com/sarfarazunarr/typescript-projects",
+        video: "/projects",
+    },
+    {
+        img: "/projects/pythonfamous.jpg",
+        title: "12 Python Projects",
+        description: "Here are 12 mini but useful projects created with Python like: Password Manager, Link Extractor, AutoSpell Checker, Pdf to Word, Screen Recorder,Message encoder etc",
+        github: "https://github.com/sarfarazunarr/python-projects",
+        video: "https://youtu.be/U4f37u-ocaA?feature=shared",
+    },
+    {
         img: "/projects/jobline.jpg",
         title: "Jobline",
         description: "Jobline is a simple jobportal website where recruiters can easily post jobs and can get applications from Jobseekers and can approve or reject them and Jobseekers can easily create thier profile and can apply for jobs posted on Jobline.",
@@ -62,8 +118,15 @@ const projectsdata = [
         description: "It is a best mini app that can analyze your code and give you a list of problems are errors with your given code using OpenAI api",
         github: "https://github.com/sarfarazunarr/coderAI/",
         video: "https://www.linkedin.com/posts/sarfarazunar_ai-openai-webdevelopment-activity-7154224794983399424-sWF2?utm_source=share&utm_medium=member_desktop",
+    },
+    {
+        img: "/projects/fullformgpt.jpg",
+        title: "Full Form GPT",
+        description: "It is a simple CLI based AI app. This will give you Fullform of given short form. For example, if you want to know fullform of 'AI' then just type 'AI' and it will give you the fullform of 'AI'. This is a perfect example of use of GPT-3 API. I have used python to create this application. This is a very useful application for everyone. Because it is very hard to remember all the fullforms of all the short forms. But with the help of this application, you can easily know the fullform of any short form. This is a very useful application for students, teachers, and also for everyone who wants to know the fullform of any short form.",
+        github: "https://github.com/sarfarazunarr/python-projects/tree/main/fullformgpt",
+        video: "https://www.linkedin.com/posts/sarfarazunar_generativeai-chatbot-gptapi-activity-7226279011704836096-FDjM?utm_source=share&utm_medium=member_desktop",
     }
 
 ]
 
-export default projectsdata;
+export default projects;
